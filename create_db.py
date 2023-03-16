c10b789cf67a159ec0d0207797003cd118aed538
emmlev0809 <119499443+emmlev0809@users.noreply.github.com>
2023-03-16 00:03:14 +0000

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,5 +1,13 @@
 from sqlalchemy import create_engine
-from models import Base
+from sqlalchemy.orm import Session
+from model import Base, EmailAddress
 
-engine = create_engine('sqlite:///library.sqlite', echo=True)
+engine = create_engine('sqlite:///emails.sqlite', echo=True)
+# engine = create_engine('sqlite:///:memory:', echo=True)
 Base.metadata.create_all(engine)
+
+with Session(engine) as sess:
+    # email_wrong = EmailAddress(email='not_correct')
+    email = EmailAddress(email='andrew.dales@ms.com')
+    sess.add(email)
+    sess.commit()
